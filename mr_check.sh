0ebed9e84960803204561879c5ee328d6abc9161
Brett Kuskie <fullaxx@gmail.com>
2020-04-14 07:50:34 -0600

    Add mr_check.sh and mr_scrub.sh

---
+++ mr_check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne "1" ]; then
+  echo "$0: <RAIDDEV>"
+  exit 1
+fi
+
+RAIDDEV="$1"
+
+if [ `id -u` -ne "0" ]; then
+  echo "Got Root?"
+  exit 2
+fi
+
+if [ -L ${RAIDDEV} ]; then
+  MD=`readlink ${RAIDDEV} | cut -d/ -f2`
+elif [ -b ${RAIDDEV} ]; then
+  MD=`basename ${RAIDDEV}`
+else
+  echo "I dont know what to do with ${RAIDDEV}!"
+  exit 3
+fi
+
+SA="/sys/block/${MD}/md/sync_action"
+if [ -f ${SA} ]; then
+  echo "Sending check to ${SA} ..."
+  echo "check" > ${SA}
+else
+  echo "${SA} not found!"
+  exit 4
+fi
---
+++ mr_scrub.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+usage()
+{
+  echo "$0: <start|status> <MAP>"
+  exit 1
+}
+
+if [ "$#" -ne "2" ]; then usage; fi
+
+case "$1" in
+   start) CMD="start"  ;;
+  status) CMD="status" ;;
+       *) usage;;
+esac
+
+if [ -r "$2" ]; then
+  MAP="$2"
+else
+  usage
+fi
+
+if [ `id -u` -ne "0" ]; then
+  echo "Got Root?"
+  exit 2
+fi
+
+BTFSBIN=`PATH="/sbin:$PATH" which btrfs`
+if [ "$?" != "0" ]; then
+  echo "Couldnt find btrfs!"
+  exit 3
+fi
+
+while read -r LINE; do
+  echo "FS:               ${LINE}"
+  ${BTFSBIN} scrub ${CMD} ${LINE}
+  echo
+done < ${MAP}
